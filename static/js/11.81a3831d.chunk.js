b01654ebea8611af2c67dace14c799fa90aed780
samsoftwaredev <gero@fastmail.com>
2021-03-15 01:12:58 -0700

    Updates

---
+++ static/js/11.81a3831d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpjumi=this.webpackJsonpjumi||[]).push([[11],{106:function(n,t,i){"use strict";i.r(t);var s=i(4);t.default=function(){return Object(s.jsx)("h2",{children:"Log In"})}}}]);
+//# sourceMappingURL=11.81a3831d.chunk.js.map
