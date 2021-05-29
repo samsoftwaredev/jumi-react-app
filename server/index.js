c7d7742e5cabb76a440d392e7920e47d89638dba
samsoftwaredev <gero@fastmail.com>
2021-05-28 17:59:34 -0700

    removed unneeded packages

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "../build")));
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
