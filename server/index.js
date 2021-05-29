1c2c7a805379a65e3a433a9a350b544979492a36
samsoftwaredev <gero@fastmail.com>
2021-05-28 18:31:42 -0700

    added and removed packages

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var path = require("path");
-var app = express();
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../build")));
+// Enabled CORS. Now anyone can access these route= require(any other domain
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
